6e35bac41dc984930bc21d9b296cbe097f9d163c
Mailis Kokla <mailis@fatarrow.net>
2021-03-29 14:43:38 +0300

    Add complete tests

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,112 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-container>
+      <v-row>
+        <v-col>
+          <h1>This is an about page</h1>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="8">
+          <v-card v-for="(post, index) in posts" :key="index" class="mb-3">
+            <v-list-item three-line>
+              <v-list-item-content>
+                <div class="overline mb-4">Post number {{ post.id }}</div>
+                <v-list-item-title class="headline mb-1">
+                  Clicked {{ post.clicked || 0 }} times
+                </v-list-item-title>
+                <v-list-item-subtitle class="headline mb-1">
+                  {{ post.title }}
+                </v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-avatar
+                tile
+                size="80"
+                color="grey"
+              ></v-list-item-avatar>
+            </v-list-item>
+            <v-card-actions>
+              <v-btn
+                outlined
+                rounded
+                text
+                @click="buttonClick(index)"
+                :id="'button-' + index"
+              >
+                Button
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col cols="4" v-if="alerts.length > 0">
+          <div v-for="(alert, index) in alerts" :key="'alert-' + index">
+            <v-alert
+              v-model="alert[index]"
+              border="left"
+              close-text="Close Alert"
+              type="success"
+              dark
+            >
+              Post number {{ alert.clickedCard }} has been clicked
+              {{ alert.clicked }}
+              {{ alert.clicked > 1 ? "times" : "time" }}
+              <template slot="close">
+                <v-icon @click="removeAlert(index)">mdi-close-circle</v-icon>
+              </template>
+            </v-alert>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
+<script>
+import axios from "axios";
+export default {
+  name: "Home",
+  data() {
+    return {
+      alerts: [],
+      posts: []
+    };
+  },
+  watch: {},
+  computed: {},
+  async created() {
+    this.getPosts();
+  },
+  methods: {
+    async getPosts() {
+      const response = await axios.get(
+        `http://jsonplaceholder.typicode.com/posts`
+      );
+      this.posts = response.data;
+    },
+    buttonClick(index) {
+      if (this.posts[index] && this.posts[index].clicked) {
+        this.posts[index].clicked++;
+      } else {
+        this.posts[index] = {
+          ...this.posts[index],
+          clicked: 1
+        };
+      }
+      this.alerts.push({
+        clickedCard: this.posts[index].id,
+        clicked: this.posts[index].clicked,
+        visible: true
+      });
+      this.removeExcessAlerts();
+    },
+    removeExcessAlerts() {
+      if (this.alerts.length > 5) {
+        this.alerts.shift();
+      }
+    },
+    removeAlert(index) {
+      this.alerts.splice(index, 1);
+    }
+  }
+};
+</script>
---
+++ tests/unit/about.spec.js
@@ -0,0 +1,87 @@
+import { shallowMount } from "@vue/test-utils";
+import About from "@/views/About.vue";
+
+const spy = jest.spyOn(About.methods, "getPosts");
+
+jest.mock("axios", () => ({
+  get: () =>
+    Promise.resolve({
+      data: [
+        {
+          userId: 1,
+          id: 1,
+          title:
+            "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+          body:
+            "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        }
+      ]
+    })
+}));
+
+describe("About.vue", () => {
+  it("should get trigger getPosts method", () => {
+    // Using spy to check if method is triggered
+    // eslint-disable-next-line no-unused-vars
+    const wrapper = shallowMount(About);
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it("should set posts with getPosts method", async () => {
+    // Using jest.mock to moch api response for axios request
+    const wrapper = shallowMount(About);
+    await wrapper.vm.$nextTick();
+    expect(wrapper.vm.posts.length).toBe(1);
+  });
+
+  it("should set alerts", async () => {
+    // Set posts like previous test
+    // Call out buttonClick and check if there is alert(s)
+    const wrapper = shallowMount(About, {});
+    await wrapper.vm.$nextTick();
+
+    wrapper.vm.buttonClick(1);
+    expect(wrapper.vm.alerts.length).toBe(1);
+
+    wrapper.vm.buttonClick(1);
+    expect(wrapper.vm.alerts.length).toBe(2);
+  });
+
+  it("should removeExcessAlerts", async () => {
+    // Set posts like previous test
+    // Call out buttonClick more than five times
+    const wrapper = shallowMount(About, {});
+    await wrapper.vm.$nextTick();
+
+    wrapper.vm.buttonClick(1);
+    wrapper.vm.buttonClick(1);
+    wrapper.vm.buttonClick(1);
+    wrapper.vm.buttonClick(1);
+    wrapper.vm.buttonClick(1);
+    wrapper.vm.buttonClick(1);
+    wrapper.vm.buttonClick(1);
+    wrapper.vm.buttonClick(1);
+
+    expect(wrapper.vm.alerts.length).toBe(5);
+  });
+
+  it("should removeAlert", async () => {
+    // Set posts like previous test
+    // Call out buttonClick couple times
+    // Check if there are as many alerts as you clicked
+    // Call out removeAlert
+    // Check if alerts length is as it should be
+    const wrapper = shallowMount(About, {});
+    await wrapper.vm.$nextTick();
+
+    wrapper.vm.buttonClick(1);
+    wrapper.vm.buttonClick(1);
+    wrapper.vm.buttonClick(1);
+
+    expect(wrapper.vm.alerts.length).toBe(3);
+
+    wrapper.vm.removeAlert(1);
+
+    expect(wrapper.vm.alerts.length).toBe(2);
+  });
+});
